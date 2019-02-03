f7e8e544fbe51a0541ec2c0ce456598adfa98c2d
linkel <ironcarbide@gmail.com>
2019-02-02 19:53:57 -0800

    server need path dependency

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 //const port = 3333;
-
+const path = require('path');
 const server = express();
 server.use(express.json());
 server.use(cors());
@@ -123,7 +123,7 @@ server.delete('/items/:id', (req, res) => {
 
 server.use(express.static(path.join(__dirname, 'client/build')));
 
-server.get('/', (req, res) => {
+server.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname+'/client/build/index.html'));
   });
 
