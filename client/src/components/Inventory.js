37ee7c29f7eb6d78fcbb85a11e33ec5f57eee70a
linkel <ironcarbide@gmail.com>
2019-02-06 12:03:46 -0800

    Item card shows red if stock is at 0

--- client/src/components/Inventory.js
+++ client/src/components/Inventory.js
@@ -8,21 +8,39 @@ const Inventory = props => {
     return (
         <div className="items-list-wrapper">
         {props.items.map(item => (
+            item.amount === 0 ?
+            <div
+            onClick={e => routeToItem(e, item)}
+            className="item-card-OUT"
+            key={item.id}
+            >
+                <img
+                    className="item-list-image-OUT"
+                    onError={props.onError}
+                    src={item.imageURL}
+                    alt={item.name}
+                />
+                <div className="text-bubble">
+                    <p><span className="quantity-OUT">{item.amount}</span> {item.unit}</p>
+                    <p>{item.name}</p>
+                </div>
+            </div>
+            :
             <div
             onClick={e => routeToItem(e, item)}
             className="item-card"
             key={item.id}
             >
-            <img
-                className="item-list-image"
-                onError={props.onError}
-                src={item.imageURL}
-                alt={item.name}
-            />
-            <div className="text-bubble">
-            <p><span className="quantity">{item.amount}</span> {item.unit}</p>
-            <p>{item.name}</p>
-            </div>
+                <img
+                    className="item-list-image"
+                    onError={props.onError}
+                    src={item.imageURL}
+                    alt={item.name}
+                />
+                <div className="text-bubble">
+                    <p><span className="quantity">{item.amount}</span> {item.unit}</p>
+                    <p>{item.name}</p>
+                </div>
             </div>
         ))}
         </div>
