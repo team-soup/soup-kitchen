3f480402b60d540805c13c40e2b86a911dd5c435
linkel <ironcarbide@gmail.com>
2019-02-05 16:53:23 -0800

    Removed the old server.

--- index.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config(); // load .env variables
-
-const server  = require('./api/server.js');
-
-const port = process.env.PORT || 8000;
-server.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
